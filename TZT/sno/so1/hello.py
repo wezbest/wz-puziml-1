826b5f7c9e290c5cb31ec50a3a21bd96dfa7ef67
wezbest <110793713+wezbest@users.noreply.github.com>
2025-02-06 12:30:36 +0000

    🫦

--- TZT/sno/so1/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from so1!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ TZT/sno/so1/panty.py
@@ -0,0 +1,10 @@
+# Main Entry Point
+from src.ut import *
+
+
+def main():
+    prjint("Hello from so1!")
+
+
+if __name__ == "__main__":
+    main()
