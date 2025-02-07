366642c4a72c951d6c9971304ba6e46ff2c1dbd7
wezbest <110793713+wezbest@users.noreply.github.com>
2025-02-07 17:01:54 +0000

    🫦

--- TZT/OPR/o1/src/ban.ts
+++ TZT/OPR/o1/src/ban.ts
@@ -1,16 +1,20 @@
 // Printing Banner Function
 
 import chalk from "chalk"
+import boxen from "boxen"
 
 const ban1 = `
-███████╗  █████╗  ██████╗  ████████╗
-██╔════╝ ██╔══██╗ ██╔══██╗ ╚══██╔══╝
-█████╗   ███████║ ██████╔╝    ██║   
-██╔══╝   ██╔══██║ ██╔══██╗    ██║   
-██║      ██║  ██║ ██║  ██║    ██║   
-╚═╝      ╚═╝  ╚═╝ ╚═╝  ╚═╝    ╚═╝   
+╔═╗ ╔═╗ ╔═╗ ╔╗╔    ╦═╗ ╔═╗ ╦ ╦ ╔╦╗ ╔═╗ ╦═╗
+║ ║ ╠═╝ ║╣  ║║║ ── ╠╦╝ ║ ║ ║ ║  ║  ║╣  ╠╦╝
+╚═╝ ╩   ╚═╝ ╝╚╝    ╩╚═ ╚═╝ ╚═╝  ╩  ╚═╝ ╩╚═
 `
 
 export function printBanner() {
-  console.log(chalk.green(ban1))
+  console.log(
+    boxen(ban1, {
+      padding: 0.5,
+      borderColor: "green",
+      borderStyle: "round",
+    })
+  )
 }
