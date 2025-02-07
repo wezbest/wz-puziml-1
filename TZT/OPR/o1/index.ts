bb6c6c6dfe82c01fe3f2c9e08a18798023fc83d8
wezbest <110793713+wezbest@users.noreply.github.com>
2025-02-07 19:12:52 +0000

    🫦

--- TZT/OPR/o1/index.ts
+++ TZT/OPR/o1/index.ts
@@ -1,11 +1,5 @@
 // Main enry point
-import { la1, la2 } from "./src/ut1"
-import {
-  comSamb1,
-  comSamb2writeToResults,
-  comSamb2writeToResultsStreaming,
-  comSamb3,
-} from "./src/fu1"
+
 import { printBanner } from "./src/ban"
 import { printOutput } from "./src/or1"
 
