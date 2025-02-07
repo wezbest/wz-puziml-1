0055979404ce1747f879fc516defc1855d85c839
wezbest <110793713+wezbest@users.noreply.github.com>
2025-02-07 19:29:40 +0000

    🫦

--- TZT/OPR/o1/src/ban.ts
+++ TZT/OPR/o1/src/ban.ts
@@ -3,26 +3,25 @@
 import chalk from "chalk"
 import boxen from "boxen"
 
-const banBig = `
+const combinedBanner = `
 ╔═╗ ╔═╗ ╔═╗ ╔╗╔    ╦═╗ ╔═╗ ╦ ╦ ╔╦╗ ╔═╗ ╦═╗
 ║ ║ ╠═╝ ║╣  ║║║ ── ╠╦╝ ║ ║ ║ ║  ║  ║╣  ╠╦╝
 ╚═╝ ╩   ╚═╝ ╝╚╝    ╩╚═ ╚═╝ ╚═╝  ╩  ╚═╝ ╩╚═
-`
-const smallText = `
+· · ────── ꒰ঌ·✦·໒꒱ ────── · ·· · ────── ꒰ঌ·✦·໒꒱ ────── · ·
 Avaialble Models:
-
+1 - "google/gemini-2.0-pro-exp-02-05:free" - Good For Coding
+2 - "google/gemini-2.0-flash-lite-preview-02-05:free" - Good For Coding
+3 - "google/gemini-2.0-flash-thinking-exp:free" - Good For Research
+4 - "deepseek/deepseek-r1-distill-llama-70b:free" - Good for Coding . Research
+5 - "deepseek/deepseek-r1:free" - Best Overall but slow,
+6 - "sophosympatheia/rogue-rose-103b-v0.2:free" - Roleplay Stories,
 `
 
-const ban1 = `
-╔═╗ ╔═╗ ╔═╗ ╔╗╔    ╦═╗ ╔═╗ ╦ ╦ ╔╦╗ ╔═╗ ╦═╗
-║ ║ ╠═╝ ║╣  ║║║ ── ╠╦╝ ║ ║ ║ ║  ║  ║╣  ╠╦╝
-╚═╝ ╩   ╚═╝ ╝╚╝    ╩╚═ ╚═╝ ╚═╝  ╩  ╚═╝ ╩╚═
-`
-const colorBan1 = chalk.magentaBright(ban1)
+const colorCombinedBanner = chalk.magentaBright(combinedBanner)
 
 export function printBanner() {
   console.log(
-    boxen(colorBan1, {
+    boxen(colorCombinedBanner, {
       padding: 0.5,
       borderColor: "green",
       borderStyle: "round",
