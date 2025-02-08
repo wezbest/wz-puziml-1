aa7f192b9a8db37c76aee1095f8914043343ac0c
wezbest <110793713+wezbest@users.noreply.github.com>
2025-02-08 14:05:59 +0000

    🫦

---
+++ TZT/BUO/b1/runz.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+# This bash srcript is for installing the KL docker image here
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+
+hea1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+bu_ru() {
+    hea1 "bun: Serving Fetch Request"
+    CO1="bun run index.ts"
+    echo -e "${GREEN}--- Running ${CO1} ---"
+    eval "$CO1"
+}
+
+# Execute the function
+bu_ru
